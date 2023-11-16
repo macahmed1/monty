6ad6df9f352dd8702813ff9737f39a1d4c9d1d40
macahmed1 <malikprog@yahoo.com>
2023-11-16 14:00:33 -0800

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - does nothing
+ * @stack: pointer to stack
+ * @line: line number
+ * Return: Nothing
+ */
+void _nop(stack_t **stack, unsigned int line)
+{
+	(void) stack;
+	(void) line;
+}
