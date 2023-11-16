e989b1fb3b6721bf563fe77c61102a7c77f33068
macahmed1 <malikprog@yahoo.com>
2023-11-16 14:26:58 -0800

    stack.c

---
+++ stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _stack - Set the mode to stacking
+ * @stack: the stack
+ * @line: line number
+ */
+void _stack(stack_t **stack, unsigned int line)
+{
+	 (void) stack;
+	 (void) line;
+	global.mode = 0;
+}
